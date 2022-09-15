9e16fe59b5f0123d7a6a993e71f86a0f5915bf31
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-09-15 14:58:13 -0300

    adiciona e configura api/mock/server

---
+++ api/mock/server/controllers/auth.js
@@ -0,0 +1,52 @@
+const data = require("../data");
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function getUser(username, password) {
+  const user = data.users.find((user) => user.username === username);
+  if (!user || password != username) {
+    console.log("login invalido!");
+    return;
+  }
+  return user;
+}
+
+function addUser(username, name, email, password) {
+  if (!password) {
+    throw "Invalid password!";
+  }
+  const id = data.users.length + 1;
+  const newUser = {
+    id,
+    username,
+    name,
+    email,
+  };
+  data.users.push(newUser);
+  return newUser;
+}
+
+module.exports = {
+  find: (req, res) => {
+    const { username, password } = req.body;
+    sleep(800).then(() => {
+      let user = getUser(username, password);
+      if (!user) {
+        res.status(404).end();
+      }
+      res.send(user);
+    });
+  },
+  add: (req, res) => {
+    try {
+      const { username, name, email, password } = req.body;
+      const newUser = addUser(username, name, email, password);
+      res.send(newUser);
+    } catch (error) {
+      res.status(404).end(error);
+    }
+  },
+};
+Footer;
---
+++ src/api/tasks.api.js
@@ -0,0 +1,28 @@
+import api from "@/api";
+
+export default {
+  getTasks: () => {
+    return new Promise((resolve, reject) => {
+      api
+        .get("/api/tasks")
+        .then((response) => {
+          return resolve(response.data);
+        })
+        .catch((error) => {
+          return reject(error);
+        });
+    });
+  },
+  removeTask: (taskId) => {
+    return new Promise((resolve, reject) => {
+      api
+        .delete(`/api/tasks/${taskId}`)
+        .then((response) => {
+          return resolve(response.data);
+        })
+        .catch((error) => {
+          return reject(error);
+        });
+    });
+  },
+};
