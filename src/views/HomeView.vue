694e393ca519798c9bec91f03f18ccbd6cb05e54
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-09-15 10:14:24 -0300

    adiciona rota para tela de login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
-  <header class="text-center">
-    <div class="indigo accent-1">
+  <v-main class="text-center">
+    <v-row class="indigo accent-1 d-flex justify-center">
       <v-icon size="180px" color="white "> mdi-format-list-checks</v-icon>
-    </div>
-    <div class="conteudo">
+    </v-row>
+    <v-container>
       <h1>ToDo Vuetify</h1>
       <p>Aplicativo de organização de tarefas e produtividade com Vuetify!</p>
-      <v-btn class="rounded-pill indigo accent-1 white--text"> Começar</v-btn>
-    </div>
-  </header>
+      <v-btn
+        class="rounded-pill indigo accent-1 white--text"
+        :to="{ name: 'login' }"
+      >
+        Começar</v-btn
+      >
+    </v-container>
+  </v-main>
 </template>
 
-<script></script>
+<script>
+export default {};
+</script>
 <style scoped></style>
