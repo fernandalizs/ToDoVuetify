e6f39bb78ba7e41d2f12b85753fe18a65cffa285
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-09-17 12:26:54 -0300

    altera cor do titulo

--- src/components/PageBar.vue
+++ src/components/PageBar.vue
@@ -7,7 +7,7 @@
   >
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-    <v-toolbar-title>ToDo List</v-toolbar-title>
+    <v-toolbar-title class="white--text">ToDo List</v-toolbar-title>
 
     <v-spacer></v-spacer>
 
