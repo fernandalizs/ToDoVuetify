e154889d6c0066d886f41c939f2dbb3505eb3e7e
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-09-20 14:42:11 -0300

    adiciona usuario e paginas na navbar

--- src/components/PageBar.vue
+++ src/components/PageBar.vue
@@ -28,18 +28,27 @@
     <v-navigation-drawer v-model="drawer" app temporary>
       <v-list-item>
         <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          <v-avatar color="#536DFE">
+            <span class="white--text text-h5">{{
+              getLoggedUser.name.at(0)
+            }}</span>
+          </v-avatar>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
+          <v-list-item-title>{{ getLoggedUser.name }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
       <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="{ name: item.routes }"
+        >
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -53,20 +62,39 @@
   </div>
 </template>
 <script>
+// import AuthApi from "@/api/auth.api.js";
+
 export default {
   data() {
     return {
       drawer: false,
       items: [
-        { title: "Home", icon: "mdi-home", to: { name: "home" } },
-        { title: "Resumo", icon: "mdi-view-dashboard", to: { name: "info" } },
+        { title: "Home", icon: "mdi-home", routes: "home" },
+        { title: "Resumo", icon: "mdi-view-dashboard", routes: "info" },
         {
           title: "Adicionar Projeto",
           icon: "mdi-folder-plus",
-          to: { name: "projectForm" },
+          routes: "projectForm",
+        },
+        {
+          title: "Tarefas",
+          icon: "mdi-format-list-checkbox",
+          routes: "taskList",
+        },
+        {
+          title: "Adicionar Tarefa",
+          icon: "mdi-playlist-plus",
+          routes: "taskForm",
         },
       ],
     };
+  },
+  computed: {
+    getLoggedUser() {
+      const userStr = localStorage.getItem("user");
+      const user = JSON.parse(userStr);
+      return user;
+    },
   },
 };
 </script>
